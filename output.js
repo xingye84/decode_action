05e2f4388eaec17aab0b7344d5c1ae3256639088
action <action@github.com>
2025-11-15 06:30:24 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,191 +1,224 @@
-//Fri Nov 07 2025 11:18:47 GMT+0000 (Coordinated Universal Time)
+//Sat Nov 15 2025 06:30:24 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-(function () {})();
 const {
-    EVENT_NAMES: _0x8f8d47,
-    request: _0x122003,
-    on: _0x55b5ee,
-    send: _0x525768
+    EVENT_NAMES,
+    request,
+    on,
+    send
   } = globalThis.lx,
-  _0x587808 = {
-    "tx": {
-      "128k": "6",
-      "320k": "8",
-      "flac": "10",
-      "flac24bit": "11"
-    },
+  CURRENT_VERSION = "1.0.0",
+  VERSION_CHECK_URL = "https://13413.kstore.vip/lxmusic/changqing.json",
+  qualitys = {
     "kg": {
-      "128k": "128k",
-      "320k": "320k",
-      "flac": "flac",
-      "flac24bit": "flac24bit"
+      "128k": "standard",
+      "320k": "exhigh",
+      "flac": "lossless"
     },
-    "kw": {
-      "128k": "128kmp3",
-      "320k": "320kmp3",
-      "flac": "2000kflac",
-      "flac24bit": "4000kflac"
+    "tx": {
+      "128k": "standard",
+      "320k": "exhigh",
+      "flac": "lossless"
     },
     "wy": {
       "128k": "standard",
       "320k": "exhigh",
-      "flac": "lossless",
-      "flac24bit": "hires"
+      "flac": "lossless"
+    },
+    "kw": {
+      "128k": "standard",
+      "320k": "exhigh",
+      "flac": "lossless"
     },
     "mg": {
-      "128k": "PQ",
-      "320k": "HQ"
+      "128k": "standard",
+      "320k": "exhigh",
+      "flac": "lossless"
     }
   },
-  _0x27a32e = (_0x309ff2, _0xc8ff30) => new Promise((_0x499e3f, _0x53131c) => {
-    _0x122003(_0x309ff2, _0xc8ff30, (_0xbee71a, _0x42709f) => {
-      if (_0xbee71a) return _0x53131c(_0xbee71a);
-      _0x499e3f(_0x42709f.body);
+  apis = {
+    "kg": {
+      "musicUrl"(_0x21abba, _0x5048a6) {
+        let _0x4a8ffc = "";
+        const _0x489330 = _0x21abba.hash;
+        console.log(_0x5048a6, _0x489330);
+        _0x4a8ffc = "https://musicapi.haitangw.net/music/kg_song%E6%8D%A2%E6%BA%90%E7%89%88.php?type=mp3&id=" + _0x489330 + "&level=" + _0x5048a6;
+        return new Promise(_0x155149 => {
+          _0x155149(_0x4a8ffc);
+        });
+      }
+    },
+    "tx": {
+      "musicUrl"(_0x537d1d, _0x25abbf) {
+        let _0x320e74 = "";
+        const _0x4336f5 = _0x537d1d.songmid;
+        console.log(_0x25abbf, _0x4336f5);
+        _0x320e74 = "https://musicapi.haitangw.net/music/qq_song_kw.php?type=mp3&id=" + _0x4336f5 + "&level=" + _0x25abbf;
+        return new Promise(_0x2f1d37 => {
+          _0x2f1d37(_0x320e74);
+        });
+      }
+    },
+    "wy": {
+      "musicUrl"(_0x1c17e0, _0x1c71e6) {
+        let _0x494aa6 = "";
+        const _0x5c5a85 = _0x1c17e0.songmid;
+        console.log(_0x1c71e6, _0x5c5a85);
+        _0x494aa6 = "https://musicapi.haitangw.net/music/wy.php?type=mp3&id=" + _0x5c5a85 + "&level=" + _0x1c71e6;
+        return new Promise(_0xae5fab => {
+          _0xae5fab(_0x494aa6);
+        });
+      }
+    },
+    "kw": {
+      "musicUrl"(_0x44d5a5, _0x404bd6) {
+        let _0x4e35d4 = "";
+        const _0x27f61b = _0x44d5a5.songmid;
+        console.log(_0x404bd6, _0x27f61b);
+        _0x4e35d4 = "https://musicapi.haitangw.net/music/kw.php?type=mp3&id=" + _0x27f61b + "&level=" + _0x404bd6;
+        return new Promise(_0x205946 => {
+          _0x205946(_0x4e35d4);
+        });
+      }
+    },
+    "mg": {
+      "musicUrl"(_0x2ac81a, _0x20fe2a) {
+        let _0x254173 = "";
+        const _0x3157b8 = _0x2ac81a.songmid;
+        console.log(_0x20fe2a, _0x3157b8);
+        _0x254173 = "https://musicapi.haitangw.net/musicapi/mg.php?type=mp3&id=" + _0x3157b8 + "&level=" + _0x20fe2a;
+        return new Promise(_0x200a7b => {
+          _0x200a7b(_0x254173);
+        });
+      }
+    }
+  },
+  compareVersions = (_0xca8739, _0x41f303) => {
+    const _0x3b4240 = _0xca8739.split(".").map(Number),
+      _0x36b60d = _0x41f303.split(".").map(Number);
+    for (let _0x4af66f = 0; _0x4af66f < Math.max(_0x3b4240.length, _0x36b60d.length); _0x4af66f++) {
+      const _0xea397 = _0x3b4240[_0x4af66f] || 0,
+        _0x16c660 = _0x36b60d[_0x4af66f] || 0;
+      if (_0xea397 > _0x16c660) return 1;
+      if (_0xea397 < _0x16c660) return -1;
+    }
+    return 0;
+  },
+  checkUpdate = async () => {
+    return new Promise((_0x5e1c36, _0x6e53c4) => {
+      request(VERSION_CHECK_URL, {
+        "method": "GET",
+        "timeout": 3000
+      }, (_0x175933, _0x463b87) => {
+        if (_0x175933 || _0x463b87.statusCode !== 200) {
+          console.log("检查更新失败:", _0x175933 || _0x463b87.statusMessage);
+          _0x5e1c36(null);
+          return;
+        }
+        try {
+          const _0x4a8189 = _0x463b87.body;
+          compareVersions(CURRENT_VERSION, _0x4a8189.version) < 0 ? _0x5e1c36({
+            "version": _0x4a8189.version,
+            "updateUrl": _0x4a8189.updateUrl,
+            "description": _0x4a8189.description || ""
+          }) : _0x5e1c36(null);
+        } catch (_0x2a0972) {
+          console.log("解析版本信息失败:", _0x2a0972);
+          _0x5e1c36(null);
+        }
+      });
     });
-  }),
-  _0x4035b7 = {
-    "tx": {
-      async "musicUrl"({
-        songmid: _0x133598
-      }, _0x532a0d) {
-        let _0x24f45f = "https://api.vkeys.cn/v2/music/tencent/geturl?mid=" + _0x133598 + "&quality=" + _0x532a0d;
-        const {
-          data: _0x2efdba
-        } = await _0x27a32e(_0x24f45f, {
-          "method": "GET"
-        });
-        if (_0x2efdba?.["url"]) return _0x2efdba.url;
-        const _0x1169a8 = {
-          "6": "128k",
-          "8": "320k",
-          "10": "flac",
-          "11": "flac24bit"
-        };
-        _0x24f45f = "https://88.lxmusic.中国/lxmusicv3/url/tx/" + _0x133598 + "/" + _0x1169a8[_0x532a0d];
-        return _0x27a32e(_0x24f45f, {
-          "method": "GET"
-        }).then(_0x31a2cc => _0x31a2cc.data);
-      }
-    },
-    "kg": {
-      "musicUrl"({
-        hash: _0x1e0823
-      }, _0x1790b8) {
-        const _0x4ffb2b = "https://88.lxmusic.中国/lxmusicv3/url/kg/" + _0x1e0823 + "/" + _0x1790b8;
-        return _0x27a32e(_0x4ffb2b, {
-          "method": "GET"
-        }).then(_0x1614e0 => _0x1614e0.data);
-      }
-    },
-    "kw": {
-      "musicUrl"({
-        songmid: _0x132559
-      }, _0x4624f6) {
-        const _0x18d59e = "https://nmobi.kuwo.cn/mobi.s?f=web&user=0&source=kwplayer_ar_8.5.5.0_jiakong.apk&type=convert_url_with_sign&br=" + _0x4624f6 + "&rid=" + _0x132559;
-        return _0x27a32e(_0x18d59e, {
-          "method": "GET"
-        }).then(_0x4cb62d => _0x4cb62d.data.url.split("?")[0]);
-      }
-    },
-    "wy": {
-      async "musicUrl"({
-        songmid: _0x2d46ef
-      }, _0x94eca1) {
-        let _0x6edfab = "https://api.cenguigui.cn/api/netease/music_v1.php?type=json&id=" + _0x2d46ef + "&level=" + _0x94eca1;
-        const {
-          data: _0x422bbe
-        } = await _0x27a32e(_0x6edfab, {
-          "method": "GET"
-        });
-        if (_0x422bbe?.["url"]) return _0x422bbe.url;
-        _0x6edfab = "https://wyapi.toubiec.cn/api/music/url";
-        const _0xa72fcf = await _0x27a32e(_0x6edfab, {
-          "method": "POST",
-          "headers": {
-            "Origin": "https://wyapi.toubiec.cn",
-            "Referer": "https://wyapi.toubiec.cn/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
-          },
-          "body": JSON.stringify({
-            "id": _0x2d46ef,
-            "level": _0x94eca1
-          })
-        });
-        if (_0xa72fcf?.["data"]?.[0]?.["url"]) return _0xa72fcf.data[0].url;
-        const _0x4a857c = {
-          "standard": "128",
-          "exhigh": "320",
-          "lossless": "999",
-          "hires": "999"
-        };
-        _0x6edfab = "https://music-api.gdstudio.xyz/api.php?types=url&source=netease&id=" + _0x2d46ef + "&br=" + _0x4a857c[_0x94eca1];
-        return _0x27a32e(_0x6edfab, {
-          "method": "GET"
-        }).then(_0x557020 => _0x557020.url);
-      }
-    },
-    "mg": {
-      async "musicUrl"({
-        copyrightId: _0x777b77
-      }, _0x2ac57b) {
-        let _0x5754f8 = "https://c.musicapp.migu.cn/MIGUM2.0/v1.0/content/resourceinfo.do?resourceType=2&copyrightId=" + _0x777b77;
-        const {
-          resource: [{
-            contentId: _0x11d382
-          }]
-        } = await _0x27a32e(_0x5754f8, {
-          "method": "GET"
-        });
-        _0x5754f8 = "https://c.musicapp.migu.cn/MIGUM3.0/strategy/listen-url/v2.3?resourceType=2&toneFlag=PQ&contentId=" + _0x11d382;
-        return _0x27a32e(_0x5754f8, {
-          "method": "GET",
-          "headers": {
-            "channel": "0140210"
-          }
-        }).then(_0x2c900b => _0x2ac57b === "HQ" ? _0x2c900b.data.url.replace("MP3_128", "MP3_320") : _0x2c900b.data.url);
-      }
-    }
   };
-_0x55b5ee(_0x8f8d47.request, ({
-  source: _0x298ca0,
-  info: _0x45c259
+on(EVENT_NAMES.request, ({
+  source: _0x20d572,
+  action: _0x4f1970,
+  info: _0x230367
 }) => {
-  return _0x4035b7[_0x298ca0].musicUrl(_0x45c259.musicInfo, _0x587808[_0x298ca0][_0x45c259.type]).catch(_0x586f09 => {
-    return Promise.reject(_0x586f09);
-  });
-});
-_0x525768(_0x8f8d47.inited, {
-  "sources": {
-    "tx": {
-      "name": "QQ音乐",
-      "type": "music",
-      "actions": ["musicUrl"],
-      "qualitys": ["128k", "320k", "flac", "flac24bit"]
-    },
-    "kg": {
-      "name": "酷狗音乐",
-      "type": "music",
-      "actions": ["musicUrl"],
-      "qualitys": ["128k", "320k", "flac", "flac24bit"]
-    },
-    "kw": {
-      "name": "酷我音乐",
-      "type": "music",
-      "actions": ["musicUrl"],
-      "qualitys": ["128k", "320k", "flac", "flac24bit"]
-    },
-    "wy": {
-      "name": "网易云音乐",
-      "type": "music",
-      "actions": ["musicUrl"],
-      "qualitys": ["128k", "320k", "flac", "flac24bit"]
-    },
-    "mg": {
-      "name": "咪咕音乐",
-      "type": "music",
-      "actions": ["musicUrl"],
-      "qualitys": ["128k", "320k"]
-    }
+  switch (_0x4f1970) {
+    case "musicUrl":
+      console.log(apis[_0x20d572].musicUrl(_0x230367.musicInfo, qualitys[_0x20d572][_0x230367.type]), _0x20d572);
+      return apis[_0x20d572].musicUrl(_0x230367.musicInfo, qualitys[_0x20d572][_0x230367.type]);
   }
+});
+checkUpdate().then(_0x2346ba => {
+  if (_0x2346ba) {
+    const _0x4ff4f0 = "发现新版本 v" + _0x2346ba.version + "\n" + (_0x2346ba.description ? "更新内容: " + _0x2346ba.description + "\n" : "") + "请更新后使用";
+    send(EVENT_NAMES.updateAlert, {
+      "log": _0x4ff4f0,
+      "updateUrl": _0x2346ba.updateUrl
+    });
+    console.log("发现新版本,需要更新,脚本将不会初始化:", _0x2346ba);
+    return;
+  } else console.log("当前已是最新版本,正常初始化"), send(EVENT_NAMES.inited, {
+    "openDevTools": false,
+    "sources": {
+      "kg": {
+        "name": "kg音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "tx": {
+        "name": "tx音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "wy": {
+        "name": "wy音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "kw": {
+        "name": "kw音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "mg": {
+        "name": "mg音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      }
+    }
+  });
+}).catch(_0x5dfa39 => {
+  console.log("检查更新出错,正常初始化:", _0x5dfa39);
+  send(EVENT_NAMES.inited, {
+    "openDevTools": false,
+    "sources": {
+      "kg": {
+        "name": "kg音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "tx": {
+        "name": "tx音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "wy": {
+        "name": "wy音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "kw": {
+        "name": "kw音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      },
+      "mg": {
+        "name": "mg音乐",
+        "type": "music",
+        "actions": ["musicUrl"],
+        "qualitys": ["128k", "320k", "flac"]
+      }
+    }
+  });
 });
